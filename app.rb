69ac1a1e88b67bbc51f9b1700fd157f40eeef70f
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-06-04 15:10:25 -0400

    removed typo file and added final code example from solution

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Started my server using Shotgun!"
   end
-
+  
 end
